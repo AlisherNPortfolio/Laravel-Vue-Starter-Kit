005ef31b66ba125ee7b5c9477e5612316f008bcc
Alisher <myemail9445@inbox.ru>
2025-04-22 16:37:41 +0500

    some resources

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,13 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/primevue@4/resources/themes/lara-light-indigo/theme.css">
-
-    <!-- PrimeIcons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/primeicons@7.0.0/primeicons.css">
-
-    <!-- PrimeFlex -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/primeflex@3.3.1/primeflex.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/primeicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/primeflex.min.css') }}">
     <title>Admin | Dashboard</title>
     @vite(['resources/css/admin/app.css'])
 </head>
