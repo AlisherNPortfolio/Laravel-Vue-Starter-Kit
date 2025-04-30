8dacb576fb4eaccb14766c11e9be6e17fb8d4aa0
Alisher <myemail9445@inbox.ru>
2025-04-30 12:08:00 +0500

    page loading status codes in vuex store

--- resources/js/admin/store/index.ts
+++ resources/js/admin/store/index.ts
@@ -2,11 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    isLoading: false,
   },
   getters: {
 
   },
   mutations: {
+    setLoadingStatus(state, status = false) {
+        state.isLoading = status
+    }
   },
   actions: {
   },
