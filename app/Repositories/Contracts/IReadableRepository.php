bbd06cdb8d10058cffdd0cec6edfbc697160a96e
Alisher <myemail9445@inbox.ru>
2025-05-07 08:15:36 +0500

    repository interfaces

---
+++ app/Repositories/Contracts/IReadableRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+interface IReadableRepository
+{
+    public function find(int $id, array $relations = []): ?Model;
+
+    public function all(array $relations = []): Collection;
+
+    public function paginate($perPage = 20): Collection;
+
+    public function getPagination(int $id, int $perPage, array $relations = []): array;
+}
